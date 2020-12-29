ce2b8e29420d9339b4426fd700f5a0a362fefbbe
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-29 11:34:04 -0600

    added sign-in, custom-button, form-input components and styling

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
-    <Header />
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in-component';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <SignIn className='sign-in-and-sign-up'>SIGN IN
+    </SignIn>
+);
+
+export default SignInAndSignUpPage;
