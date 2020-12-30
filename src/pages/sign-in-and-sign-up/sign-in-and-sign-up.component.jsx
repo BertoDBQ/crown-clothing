80063a51ca53366b0ac8326445087878b4d4a725
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-30 14:57:16 -0600

    implemented firebase utils, ability to create user

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in-component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-    <SignIn className='sign-in-and-sign-up'>SIGN IN
-    </SignIn>
+    <div className='sign-in-and-sign-up'>SIGN IN
+        <SignIn />
+        <SignUp />
+    </div>
 );
 
 export default SignInAndSignUpPage;
