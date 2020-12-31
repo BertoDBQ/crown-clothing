a3cfc88b25bedab11937fd7875d9bea79a623c1a
Bob Ahlrichs <ahlrichs@cartegraph.com>
2020-12-30 18:15:23 -0600

    updating to redirect to home if signed in

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -6,7 +6,7 @@ import SignUp from '../../components/sign-up/sign-up.component';
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'>SIGN IN
+    <div className='sign-in-and-sign-up'>
         <SignIn />
         <SignUp />
     </div>
