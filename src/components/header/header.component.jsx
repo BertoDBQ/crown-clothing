1879c51440297f28f8fd7ee35e6388cbeb6623b7
Bob Ahlrichs <ahlrichs@cartegraph.com>
2021-01-03 19:25:19 -0600

    componment

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../../src/assets/crown.svg';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -20,6 +22,7 @@ const Header = ({ currentUser }) => (
                 ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> 
                 : <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 )
