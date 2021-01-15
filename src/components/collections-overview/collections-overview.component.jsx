3980b5c52cdc97cf4be953245dcee9a5f9f29563
Bob Ahlrichs <ahlrichs@cartegraph.com>
2021-01-14 20:33:10 -0600

    added collections overview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 import './collections-overview.styles.scss';
@@ -19,7 +19,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
 
 const selectShop = state => state.shop;
 
@@ -6,3 +7,23 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+// using memoize
+export const selectCollection = memoize((collectionUrlParam) => 
+    createSelector(
+        [selectCollections],
+        (collections) => collections[collectionUrlParam]
+    )
+);
+
+// not using memoize
+// export const selectCollection = (collectionUrlParam) => 
+//     createSelector(
+//         [selectCollections],
+//         collections => collections[collectionUrlParam]
+//     )
