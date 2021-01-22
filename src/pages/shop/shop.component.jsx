a58199a07f9ab40f33db83f509a4991047c5e1da
Bob Ahlrichs <ahlrichs@cartegraph.com>
2021-01-22 16:49:36 -0600

    added asyc redux (thunk) for the shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import { updateCollections } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionsFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+//import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
 // using HOC WithSpinner
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
@@ -15,55 +17,72 @@ const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
 
-    state = {
-        loading: true
-    }
-
-    unsubscribeFromSnapshot = null;
-
+    // componentDidMount fires after the initial render
+    // this is where you want your api calls
     componentDidMount() {
-        const { updateCollections } = this.props;
-        const collectionRef = firestore.collection('collections');
-
-        // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
-        //     // console.log(snapshot);
-        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        //     console.log(collectionsMap);
-        //     updateCollections(collectionsMap);
-        //     this.setState( {loading: false });
-        // });
-
-        // using promises against firebase
-        collectionRef.get()
-        .then(snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            this.setState( {loading: false });
-        });
-        
-        // // using firebase native fetch api
-        // fetch('https://firestore.googleapis.com/v1/projects/crown-clothing-42605/databases/(default)/documents/collections')
-        // .then(response => response.json())
-        // .then(collections => console.log(collections));
+        const{ fetchCollectionsStartAsync} = this.props;
+        fetchCollectionsStartAsync();
     }
+
+    // commented this all out when using redux thunk in the shop reducer/action
+    // state = {
+    //     loading: true
+    // }
+
+    // unsubscribeFromSnapshot = null;
+
+    // componentDidMount() {
+    //     const { updateCollections } = this.props;
+    //     const collectionRef = firestore.collection('collections');
+
+    //     // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    //     //     // console.log(snapshot);
+    //     //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     //     console.log(collectionsMap);
+    //     //     updateCollections(collectionsMap);
+    //     //     this.setState( {loading: false });
+    //     // });
+
+    //     // // using promises against firebase
+    //     // collectionRef.get()
+    //     // .then(snapshot => {
+    //     //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //     //     updateCollections(collectionsMap);
+    //     //     this.setState( {loading: false });
+    //     // });
+        
+    //     // // using firebase native fetch api
+    //     // fetch('https://firestore.googleapis.com/v1/projects/crown-clothing-42605/databases/(default)/documents/collections')
+    //     // .then(response => response.json())
+    //     // .then(collections => console.log(collections));
+    // }
 
     render() {
-        const { match } = this.props;
-        const { loading } = this.state;
+        const { match, isCollectionsFetching, isCollectionLoaded  } = this.props;
+        // const { loading } = this.state;
         return (
             <div className='shop-page'>
                 <Route exact path={`${match.path}`} 
-                    render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
+                    render={props => <CollectionsOverviewWithSpinner isLoading={isCollectionsFetching} {...props} />} />
                 <Route path={`${match.path}/:collectionId`} 
-                    render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+                    render={props => <CollectionPageWithSpinner isLoading={!isCollectionLoaded} {...props} />} />
             </div>
         )
     };
 };
 
-const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => 
-        dispatch(updateCollections(collectionsMap))
+const mapStateToProps = createStructuredSelector({
+    isCollectionsFetching: selectIsCollectionsFetching,
+    isCollectionLoaded: selectIsCollectionsLoaded
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+});
+
+// const mapDispatchToProps = dispatch => ({
+//     updateCollections: collectionsMap => 
+//         dispatch(updateCollections(collectionsMap))
+// });
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,6 +8,16 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
+export const selectIsCollectionsFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+)
+
+export const selectIsCollectionsLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections
+)
+
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
     collections => collections 
